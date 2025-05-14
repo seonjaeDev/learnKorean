5c087c5755e5f967340bb685304183cad139f5e4
mangoDev <valdonjefferson@outlook.com>
2025-05-14 13:06:29 +0800

    init

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+function showSections() {
+  // document.getElementById("nav").style.top = "0";
+  let sect00 = document.getElementById("sect00")
+  sect00.style.height = "100%";
+  console.log("showSections");
+}
