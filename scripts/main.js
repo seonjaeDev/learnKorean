ed9918d6efb7e6ccb8cc1d49a705fc0859a6da52
seonjaeDev <valdonjefferson@outlook.com>
2025-05-14 14:20:15 +0800

    wip: section content added

--- scripts/main.js
+++ scripts/main.js
@@ -2,5 +2,6 @@ function showSections() {
   // document.getElementById("nav").style.top = "0";
   let sect00 = document.getElementById("sect00")
   sect00.style.height = "100%";
+  sect00.style.transition = "1.3s"
   console.log("showSections");
 }
